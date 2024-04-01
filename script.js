85225ec472da5ecbad2c2a9762ea8e7f03807bf5
falskuyyy <ahmdfalah10@gmail.com>
2024-04-01 20:49:38 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function showAlert() {
+    alert(Date(), tampil(), show());
+    Date();
+  
+    let tanggal = new date();
+    documentge.getElementByid("hasil").innerHTML = tanggal;
+  
+    function tampil() {
+      alert(
+        "https://linktr.ee/bergpuntang8"
+      );
+    }
+  
+    function show() {
+      alert("VJM4+G93, Jl. Gn. Puntang, Pasirmulya, Kec. Banjaran, Kabupaten Bandung, Jawa Barat 40377 ");
+    }
+  }
